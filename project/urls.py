cc6b98bf9f6cb4e0d470c54edfe07e28e6960cc3
Milova <milova_evg@mail.ru>
2016-10-16 18:31:53 +0300

    Questions

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+from welcome.views import index, health, detail, results, vote, detail 
 
 urlpatterns = [
     # Examples:
@@ -11,4 +11,8 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<question_id>[0-9]+)/$', detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', vote, name='vote'),
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', detail, name='detail'),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -1,16 +1,29 @@
 import os
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader
 
 from . import database
-from .models import PageView
+from .models import PageView,Question, Choice
 
 # Create your views here.
 
 def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('welcome/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+    return HttpResponse(output)
+    return render(request, 'welcome/index.html', context)
 
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
@@ -20,3 +33,41 @@ def index(request):
 
 def health(request):
     return HttpResponse(PageView.objects.count())
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'welcome/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'welcome/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'welcome/results.html', {'question': question})
